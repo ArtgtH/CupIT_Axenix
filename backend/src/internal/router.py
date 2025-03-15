80267ada1607c6f53dcdae22f7621af385cd0774
artem <artem2004920@gmail.com>
2025-03-15 14:06:25 +0300

    формат

--- backend/src/internal/router.py
+++ backend/src/internal/router.py
@@ -15,28 +15,29 @@ router = APIRouter(prefix="/api", tags=["api"])
 
 
 async def get_redis() -> AsyncGenerator:
-	print(REDIS_URL)
-	redis_client = redis.Redis.from_url(REDIS_URL)
-	try:
-		yield redis_client
-	finally:
-		redis_client.close()
+    redis_client = redis.Redis.from_url(REDIS_URL)
+    try:
+        yield redis_client
+    finally:
+        redis_client.close()
 
 
 @router.post("/request", response_model=Union[MessageResponse, ScheduleResponse])
-async def process_user_request(user_request: TaskRequest, redis_client=Depends(get_redis)):
-	text = user_request.text
-	session_id = str(user_request.id)
+async def process_user_request(
+    user_request: TaskRequest, redis_client=Depends(get_redis)
+):
+    text = user_request.text
+    session_id = str(user_request.id)
 
-	previous_messages_raw = await redis_client.lrange("messages", 0, -1)
-	previous_messages = [RedisMessage.parse_raw(msg) for msg in previous_messages_raw]
+    previous_messages_raw = await redis_client.lrange("messages", 0, -1)
+    previous_messages = [RedisMessage.parse_raw(msg) for msg in previous_messages_raw]
 
-	message = RedisMessage(role=Role.user, state="Уточняем", text=text)
-	await redis_client.rpush(session_id, message.json())
+    message = RedisMessage(role=Role.user, state="Уточняем", text=text)
+    await redis_client.rpush(session_id, message.json())
 
-	res = talk_with_god(text, previous_messages)
-	if isinstance(res, MessageResponse):
-		answer = RedisMessage(role=Role.ai, state="Уточняем", text=res.text)
-		await redis_client.rpush(session_id, answer.json())
+    res = talk_with_god(text, previous_messages)
+    if isinstance(res, MessageResponse):
+        answer = RedisMessage(role=Role.ai, state="Уточняем", text=res.text)
+        await redis_client.rpush(session_id, answer.json())
 
-	return res
+    return res
